67b0720056aebf9d60360584a1290c75f27a8c1f
kurohuku <kurohuku7@gmail.com>
2020-11-16 05:40:55 +0900

    Fix initialization

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,33 +12,53 @@ public class GameController : MonoBehaviour
     public GameObject rareWatch;
     public GameObject epicWatch;
     public GameObject legendaryWatch;
+    public GameObject WatchContainer;
 
     private GameObject watchSocket = null;
     private GameObject autoloadedWatch = null;
 
     private IEnumerator Start()
     {
-        // TODO: detect initialize event.
-        yield return new WaitForSeconds(5);
-        watch.ToggleWatch(true);
-        yield return new WaitForSeconds(3);
+        WatchContainer.SetActive(false);
 
-        (watchSocket, autoloadedWatch) = GetWatchObjects();
+        // This program uses the default watch as changable watch (watchSocket).
+        // Toggle to default watch.
+        watch.ToggleWatch(true);
+
+        // Wait YUR watch setup and toggle.
+        while (watchSocket == null || autoloadedWatch == null)
+        {
+            yield return new WaitForSeconds(1);
+            (watchSocket, autoloadedWatch) = GetWatchObjects();
+        }
 
         // Switch to default watch object (watch socket) to change watch type.
         autoloadedWatch.SetActive(false);
         watchSocket.SetActive(true);
 
-        // Initial watch
+        // First watch is common.
+        WatchContainer.SetActive(true);
         ChangeWatch(commonWatch);
     }
 
-    private (GameObject, GameObject) GetWatchObjects()
+    private bool IsWatchInitialized()
     {
         var watchComps = watch.GetComponentsInChildren<WatchReferenceContainer>(true);
         if (watchComps.Length < 2)
         {
-            Debug.LogException(new Exception("Default and autoloaded watches are not created!"));
+            return false;
+        }
+        return true;
+    }
+
+    private (GameObject, GameObject) GetWatchObjects()
+    {
+        // TODO: dupricated code avobe.
+        var watchComps = watch.GetComponentsInChildren<WatchReferenceContainer>(true);
+        if (watchComps.Length < 2)
+        {
+            // Default and autoloaded watches are not created.
+            return (null, null);
         }
         foreach (var watchComp in watchComps)
         {
@@ -69,6 +89,8 @@ public class GameController : MonoBehaviour
         Instantiate(newMesh.gameObject, currentMesh.parent);
         Destroy(currentMesh.gameObject);
         
+        // TODO: Copy particle system of the legendary watch.
+
         var goOnlineEvent = toWatch.GetComponent<WatchReferenceContainer>().GoOnlineEvent;
         var goOfflineEvent = toWatch.GetComponent<WatchReferenceContainer>().GoOfflineEvent;
         var watchReferenceContainer = watchSocket.GetComponent<WatchReferenceContainer>();
